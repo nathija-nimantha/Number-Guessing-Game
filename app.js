41d3bb42c2cf8bfccf4b96ac32d9ffb68df5da41
nathija-nimantha <nathija.max99@gmail.com>
2024-06-26 15:55:47 +0530

    Added images

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 let numberInput = document.getElementById('number');
 let guessBtn = document.getElementById('btnGuess');
 let result = document.getElementById('guess');
+let img = document.getElementById('imgDisplay');
 
 let randomNumber = Math.floor((Math.random()*10)+1);
 let guessCount = 1;
+let imgdis = '';
 
 guessBtn.addEventListener('click', function(){
     let guess = parseInt(numberInput.value);
@@ -13,14 +15,27 @@ guessBtn.addEventListener('click', function(){
         guessCount = 1;
         randomNumber = Math.floor((Math.random()*10)+1);
         result.innerHTML = 'Game Over! Start Again!';
+        imgdis = 'you-failed.jpg';
+        displayImage();
     }else if(guess === randomNumber){
         result.innerHTML = 'Congratulations! You guessed it in ' + guessCount + ' guesses! \n But you get nothing :) ';
         guessCount = 1;
+        imgdis = 'you-won.jpg';
+        displayImage();
     }else if(guess > randomNumber){
         result.innerHTML = 'Lower';
         guessCount++;
+        imgdis = 'try-again.jpg';
+        displayImage();
     }else{
         result.innerHTML = 'Higher';
         guessCount++;
+        imgdis = 'try-again.jpg';
+        displayImage();
     }
 });
+
+function displayImage(){
+    img.src = 'img/' + imgdis;
+}
+
